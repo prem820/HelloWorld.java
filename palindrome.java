0ea3a3ef28b291cbd6da62a9deb579553ec4dfb5
prem820 <bbanothu4@gmail.com>
2025-09-10 12:18:05 +0530

    first commit

--- palindrome.java
+++ palindrome.java
@@ -1,21 +1,16 @@
-public class palindrome 
-{
-    public static void main(String[] args) 
-    {
+public class palindrome {
+    public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Please provide a word to check!");
+            System.out.printl("Please provide a word to check!");
             return;
         }
-
         String word = args[0];  // Take word from argument
-
         if (isPalindrome(word)) {
             System.out.println(word + " is a palindrome!");
         } else {
             System.out.println(word + " is NOT a palindrome!");
         }
     }
-
     public static boolean isPalindrome(String str) {
         String reversed = new StringBuilder(str).reverse().toString();
         return str.equalsIgnoreCase(reversed);
